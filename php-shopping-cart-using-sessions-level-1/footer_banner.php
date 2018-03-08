3fdb2e734d4384687ab74b28af82696aec2a3ca0
pacmanw2 <smosh_rules3@live.com>
2018-03-08 02:28:23 -0800

    added more stuff

---
+++ php-shopping-cart-using-sessions-level-1/footer_banner.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<style>
+    .footer {
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        background-color: red;
+        color: white;
+        text-align: center;
+    }
+</style>
+
+<div class="footer">
+    <p>Footer</p>
+</div>
+
+</head>
+</html>
